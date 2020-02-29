b15a5c0e39e7bdde44b5e6f539b49e5587d83aec
Kris Monroe <krisdmonroe@gmail.com>
2020-02-29 14:09:00 -0800

    Added restricted without a token

--- api/server.js
+++ api/server.js
@@ -3,16 +3,13 @@ const express = require("express")
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require("../users/users-router.js");
+const restricted = require('../auth/auth-router.js')
 
 const server = express();
 
 server.use(express.json())
 
-server.use("/api/users", usersRouter);
+server.use("/api/users", restricted, usersRouter);
 server.use('/api/auth', authRouter);
-
-server.get("/", (req, res) => {
-    res.send("its alive")
-})
 
 module.exports = server;
